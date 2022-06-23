dd855f70024915f94398d4c22323cf59b17349f7
Mykola.Matsishin <mykola.matsishin@avispl.com>
2022-06-23 21:25:08 +0300

    do not forget to add to starter one bean and 2 dependency

--- src/main/java/com/example/kuber/backend/BackendApplication.java
+++ src/main/java/com/example/kuber/backend/BackendApplication.java
@@ -2,6 +2,9 @@ package com.example.kuber.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -10,4 +13,8 @@ public class BackendApplication {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
+	@Bean
+	public ObjectMapper objectMapper() {
+		return new ObjectMapper();
+	}
 }
