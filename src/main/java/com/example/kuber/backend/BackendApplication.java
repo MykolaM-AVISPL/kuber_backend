23657aba1b7b634d034d7ed8fb0cdc9dfbecd24d
Mykola.Matsishin <mykola.matsishin@avispl.com>
2022-06-27 11:18:03 +0300

    clean up

--- src/main/java/com/example/kuber/backend/BackendApplication.java
+++ src/main/java/com/example/kuber/backend/BackendApplication.java
@@ -3,23 +3,11 @@ package com.example.kuber.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.json.JsonMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-
-@SpringBootApplication(exclude = { RedisAutoConfiguration.class})
+@SpringBootApplication(exclude = { RedisAutoConfiguration.class })
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
-	}
-
-	@Bean
-	public ObjectMapper objectMapper() {
-		return JsonMapper.builder().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-				.addModule(new JavaTimeModule()).build();
 	}
 }
