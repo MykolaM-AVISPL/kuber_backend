b90494e8589bd0ede03cfa09e2f669dc41a76ba2
Mykola.Matsishin <mykola.matsishin@avispl.com>
2022-06-28 18:58:05 +0300

    helm

--- src/main/java/com/example/kuber/backend/BackendApplication.java
+++ src/main/java/com/example/kuber/backend/BackendApplication.java
@@ -2,9 +2,8 @@ package com.example.kuber.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 
-@SpringBootApplication(exclude = { RedisAutoConfiguration.class })
+@SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
